c0c600a4eb9b4cf1752dd710d05c5ee0fb7a700f
Jennie <jenniebuitrago@gmail.com>
2020-11-16 20:54:33 -0300

    Updates

---
+++ precache-manifest.48f6f5e53b6a7d40c1a235f5a4805577.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b154fe2e3d9083dd6cbfde56c74aba2c",
+    "url": "/Memory-emoji/index.html"
+  },
+  {
+    "revision": "bc645b9143220d9450dd",
+    "url": "/Memory-emoji/static/css/main.e5794f88.chunk.css"
+  },
+  {
+    "revision": "e4c2fb6107383841cd68",
+    "url": "/Memory-emoji/static/js/2.2ddaf1e9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Memory-emoji/static/js/2.2ddaf1e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc645b9143220d9450dd",
+    "url": "/Memory-emoji/static/js/main.7f0ff26c.chunk.js"
+  },
+  {
+    "revision": "b10dedd3bca42bdc804f",
+    "url": "/Memory-emoji/static/js/runtime-main.fb9e1e58.js"
+  },
+  {
+    "revision": "af1e0f1cb1992bb6c30ddd05d7211b88",
+    "url": "/Memory-emoji/static/media/crying.af1e0f1c.jpg"
+  },
+  {
+    "revision": "9188e7ee22543e665cad7652f3ab26c9",
+    "url": "/Memory-emoji/static/media/death.9188e7ee.jpg"
+  },
+  {
+    "revision": "2682323a5775190fffcae307aed98f42",
+    "url": "/Memory-emoji/static/media/flirting.2682323a.png"
+  },
+  {
+    "revision": "e0946023463487c4e15212d39f90d9f7",
+    "url": "/Memory-emoji/static/media/glasses.e0946023.jpg"
+  },
+  {
+    "revision": "06e3b5c34b30557fdb3aa30a44524c76",
+    "url": "/Memory-emoji/static/media/kiss.06e3b5c3.jpg"
+  },
+  {
+    "revision": "97c6900450bb8ca669c877d41d3d17ce",
+    "url": "/Memory-emoji/static/media/no-smouth.97c69004.jpg"
+  },
+  {
+    "revision": "c5e70462a03a6bde4c77788c84228a24",
+    "url": "/Memory-emoji/static/media/pokerface.c5e70462.png"
+  },
+  {
+    "revision": "653887e779d48a2c011cc18b51b9f205",
+    "url": "/Memory-emoji/static/media/rolling-eyes.653887e7.jpg"
+  },
+  {
+    "revision": "3f76cc893f12126c8261840e15e9cb66",
+    "url": "/Memory-emoji/static/media/sad.3f76cc89.png"
+  },
+  {
+    "revision": "0b0745c178f14017b377ab2146219571",
+    "url": "/Memory-emoji/static/media/sleeping.0b0745c1.jpg"
+  },
+  {
+    "revision": "10fb75f08e985ce49fe90fa6b44cc7d7",
+    "url": "/Memory-emoji/static/media/smile.10fb75f0.jpg"
+  },
+  {
+    "revision": "8a59e158c707484aae42f8eb08553d06",
+    "url": "/Memory-emoji/static/media/sweat.8a59e158.jpg"
+  },
+  {
+    "revision": "f3bfb949d95fa83f37e6cb56a4e93489",
+    "url": "/Memory-emoji/static/media/upset.f3bfb949.jpg"
+  },
+  {
+    "revision": "326b038b1e4bc748645fd9cdc0352226",
+    "url": "/Memory-emoji/static/media/wink.326b038b.jpg"
+  }
+]);
